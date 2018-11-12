a18949c9cf51d1fdeb89a880ba28ed1121414d5c
R'n'B <9968920+RnBDeveloper@users.noreply.github.com>
2018-11-12 14:04:48 +0100

    Create Datach02.java

---
+++ Datach02.java
@@ -0,0 +1,17 @@
+public class Datach02 {
+	
+	public static void main(String[] args) {
+  
+		String day = "Monday";
+		String month = "November";
+		int year = 2018;
+		int date = 12;
+    
+		System.out.println("American format:");
+		System.out.println(day+", "+month+" "+date+", "+year);
+		System.out.println("\nEuropean format:");
+		System.out.println(day+" "+date+" "+month+" "+year);
+		
+	}
+
+}
