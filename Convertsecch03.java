34dd6380121b3b7cebdb28453cc461782a74af77
R'n'B <9968920+RnBDeveloper@users.noreply.github.com>
2018-11-28 15:06:48 +0100

    Create Convertsecch03.java

---
+++ Convertsecch03.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Convertsecch03 {
+public static void main(String[] args) {
+	int h, sec, min, remainder;
+	Scanner in = new Scanner(System.in);
+	System.out.print("What's the number of seconds? ");
+	sec = in.nextInt();
+	h = sec / 3600;
+	min = (sec % 3600 ) / 60;
+  remainder = ( sec % 60 ) % 60;
+	System.out.printf("\n%d seconds = %d hours, %d minutes, & %d seconds!", sec, h, min, remainder);
+	
+	
+}
+
+}
