130a9edf381d6fb148e2822e7d1bce725a9ac01f
R'n'B <9968920+RnBDeveloper@users.noreply.github.com>
2018-11-28 01:20:32 +0100

    Create Inputch03.java

---
+++ Inputch03.java
@@ -0,0 +1,11 @@
+public class Inputch03 {
+//Just an example
+	public static void main(String[] args) {
+		double pi = 3.14159;
+		double x = (int) pi * 20.0;
+		System.out.println(System.out);
+		System.out.println(4.0 / 3.0);
+		System.out.printf("\nFour thirds = %.3f\n\n", 4.0 / 3.0);
+		System.out.println(x);
+	}
+}
