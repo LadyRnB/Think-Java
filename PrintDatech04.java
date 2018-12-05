a66baadce09a45ccbb87e6681f3cdb1f8d0f7716
R'n'B <9968920+RnBDeveloper@users.noreply.github.com>
2018-12-05 21:05:14 +0100

    Create PrintDatech04.java

---
+++ PrintDatech04.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class PrintDatech04 {
+	public static void printAmerican(int date, int year, String day, String month) {
+		System.out.print("The date in American format: ");
+		System.out.println(day+", "+month+" "+date+", "+year);
+		
+	}
+	public static void printEuropean(int date, int year, String day, String month) {
+		System.out.print("The date in European format: ");
+		System.out.println(day+" "+date+" "+month+" "+year);
+	}
+	public static void main(String[] args) {
+		int date, year;
+		String day, month;
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter the day: ");
+		day = in.nextLine();
+		System.out.print("Enter the date: ");
+		date = in.nextInt();
+		in.nextLine();
+		System.out.print("Enter the month: ");
+		month = in.nextLine();
+		System.out.print("Enter the year: ");
+		year = in.nextInt();
+		System.out.println();
+		printAmerican(date, year, day, month);
+		System.out.println();
+		printEuropean(date, year, day, month);
