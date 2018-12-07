faf9464704e0ea55ffc8c142d9d779acb0065ea9
R'n'B <9968920+RnBDeveloper@users.noreply.github.com>
2018-12-07 04:50:15 +0100

    Update BeerBottlech05.java

--- BeerBottlech05.java
+++ BeerBottlech05.java
@@ -1,17 +1,25 @@
 import java.util.Scanner;
 public class BeerBottlech05 {
+	
+	public static void beforeLastVerse(int p) {
+		System.out.println(p + " bottles of beer on the wall,\r\n"+p+ 
+				" bottles of beer,\r\n" + 
+				"ya' take one down, ya' pass it around,\r\n" +
+				"no bottles of beer on the wall.");
+		System.out.println();
+	}
+	public static void endingVerse() {
+		System.out.println("No bottles of beer on the wall,\r\n" + 
+				"no bottles of beer,\r\n" + 
+				"ya' can't take one down, ya' can't pass it around,\r\n" + 
+				"'cause there are no more bottles of beer on the wall!");
+	}
 	public static void countdownbeer(int n) {
 		if (n==0) {
-			System.out.println("No bottles of beer on the wall,\r\n" + 
-					"no bottles of beer,\r\n" + 
-					"ya' can't take one down, ya' can't pass it around,\r\n" + 
-					"'cause there are no more bottles of beer on the wall!");
+			endingVerse();
 		}
 		else if ((n-1) == 0 ) {
-			System.out.println(n + " bottles of beer on the wall,\r\n"+n+ 
-					" bottles of beer,\r\n" + 
-					"ya' take one down, ya' pass it around,\r\n" +
-					"no bottles of beer on the wall.");
+			beforeLastVerse(n);
 			System.out.println();
 			countdownbeer (n-1);
 		}
@@ -37,5 +45,7 @@ public class BeerBottlech05 {
 		countdownbeer(x);
 		
 	}
+
+}
 
 }
