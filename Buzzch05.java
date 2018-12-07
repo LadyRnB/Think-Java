8693af19b74174a9cca87fedfe7ee081a24bbe56
R'n'B <9968920+RnBDeveloper@users.noreply.github.com>
2018-12-07 16:18:45 +0100

    Update Buzzch05.java

--- Buzzch05.java
+++ Buzzch05.java
@@ -7,6 +7,7 @@ public class Buzzch05 {
 		if (flag < 0) {
 		System.out.println(quince + " zoop");
 		} else {
+			//Executed just once afetr having added numbers in front of the first line of execution, & the second one!!
 		System.out.println("1ik");
 		baffle(quince);
 		System.out.println("boo-wa-ha-ha");
