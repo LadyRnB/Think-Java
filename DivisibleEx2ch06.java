24397b4fc5e5c450e457ae9e9572cae3726bea99
R'n'B <9968920+RnBDeveloper@users.noreply.github.com>
2018-12-12 23:27:25 +0100

    Create DivisibleEx2ch06.java

---
+++ DivisibleEx2ch06.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class DivisibleEx2ch06 {
+	public static boolean isDivisible(int n, int m) {
+		if (n % m == 0) {
+			return true;
+		} else { return false;
+		}
+			
+		}
+	public static void main(String[] args) {
+		int x, y;
+		Scanner in = new Scanner(System.in);
+		
+		System.out.print("Enter the number x: ");
+		x = in.nextInt();
+		System.out.print("Enter the number y: ");
+		y = in.nextInt();
+		if (isDivisible(x,y)) {
+			System.out.print(x + " is divisible by " + y);
+			System.out.println(", & its quotient is: " + x/y);
+		}
+		else {
+			System.out.print(x + " is not divisible by " + y);
+			System.out.print(", & its quotient is: "+ x/y);
+		}
+		}
+	}
