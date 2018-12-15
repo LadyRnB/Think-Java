ef51a81465907e907daced2c6b1c092c7fd835fd
R'n'B <9968920+RnBDeveloper@users.noreply.github.com>
2018-12-15 18:21:37 +0100

    Create AckCh06.java

---
+++ AckCh06.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class AckCh06 {
+	
+	public static int ack(int m, int n) {
+	if (m == 0) {
+		return (n + 1);
+	} else if (m > 0 && n == 0) {
+		int r = ack(m - 1, 1);
+		return r;
+		} 
+	else { 
+		return ack(m - 1, ack(m, n - 1));
+	}
+	}
+	
+	public static void main(String[] args) {
+		int x, y;
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter x: ");
+		x = in.nextInt();
+		System.out.print("Enter y: ");
+		y = in.nextInt();
+		System.out.print(ack(x, y));		
+	}
+	}
