291c89ad4ebcc9915e42b8be9d5717e45dc079ef
R'n'B <9968920+RnBDeveloper@users.noreply.github.com>
2018-12-15 14:44:29 +0100

    Create OddSumCh06.java

---
+++ OddSumCh06.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class OddSumCh06 {
+	
+	public static boolean isPositive (int a) {
+		boolean pos;
+	if (a > 0 ) {
+		pos = true;
+	} else {pos = false;
+	}return pos;
+	}
+  
+	public static boolean isOdd (int a) {
+		boolean od;
+		if (a % 2 != 0) {
+			od = true;
+		} 
+		else {
+			od = false;
+		} return od;
+	}
+
+	public static int oddSum(int m, int n) {
+		isPositive(n);
+		isOdd(n);
+		if (m == n) {
+			return n;
+		} 
+		else {
+			return n + (oddSum(m, n-2));
+			}
+		}
+	
+	public static void main(String [] args) {
+		int x, y;
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter x: ");
+		x = in.nextInt();
+		System.out.print("Enter y: ");
+		y = in.nextInt();
+		System.out.print("The sum of odd numbers of "+ x + " is: ");
+		System.out.print(oddSum(y, x));
+		}
+}
