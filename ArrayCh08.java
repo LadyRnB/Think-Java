12775dbb2c4aa73f80ec756fc8df78468d5f361b
R'n'B <9968920+RnBDeveloper@users.noreply.github.com>
2018-12-21 20:25:19 +0100

    Create ArrayCh08.java

---
+++ ArrayCh08.java
@@ -0,0 +1,36 @@
+
+/**
+ * A program that displays the elements of a given array
+ * raised in a given power (inside main, without method)
+ */
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class ArrayCh08 {
+	public static void main(String[] args) {
+		int n, i;
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter the number of the elements of a: ");
+		n = in.nextInt();
+		double[] a = new double[n];
+		double[] b = new double[a.length];
+
+		for (i = 0; i < a.length; i++) {
+			System.out.print("Enter the element number " + (i + 1) + " of the array: ");
+			a[i] = in.nextDouble();
+		}
+
+		System.out.println("\nThis is the array a: ");
+		System.out.print(Arrays.toString(a));
+
+		for (i = 0; i < a.length; i++) {
+			b[i] = Math.pow(a[i], 2);
+		}
+
+		System.out.println();
+		System.out.println("\nThis is the array b, \"a squared\": ");
+		System.out.print(Arrays.toString(b));
+
+		in.close();
+	}
+}
