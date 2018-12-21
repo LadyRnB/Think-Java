7041489236f2255cb2668c73b46ce4228ef5c66b
R'n'B <9968920+RnBDeveloper@users.noreply.github.com>
2018-12-21 20:31:08 +0100

    Create ArrayEx1Ch08.java
    This is another solution for this same program: https://github.com/RnBDeveloper/Think-Java/blob/master/ArrayCh08.java

---
+++ ArrayEx1Ch08.java
@@ -0,0 +1,39 @@
+
+/**
+ * A program that displays the elements of a given array
+ * raised in a given power (using a method)/ Check ArrayCh08
+ */
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class abc {
+	public static double[] powArray(double[] m) {
+		int i;
+		double[] t = new double[m.length];
+		for (i = 0; i < m.length; i++) {
+			t[i] = Math.pow(m[i], 2);
+		}
+		return t;
+	}
+
+	public static void main(String[] args) {
+		int n, i;
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter the number of the elements of a: ");
+		n = in.nextInt();
+		double[] a = new double[n];
+		double[] b = new double[a.length];
+		for (i = 0; i < a.length; i++) {
+			System.out.print("Enter the element number " + (i + 1) + " of the array: ");
+			a[i] = in.nextDouble();
+		}
+
+		System.out.println("\nThis is the array a: ");
+		System.out.print(Arrays.toString(a));
+		System.out.println();
+		System.out.println("\nThis is the array b, \"squared a elements\": ");
+		b = powArray(a);
+		System.out.print(Arrays.toString(b));
+		in.close();
+	}
+}
