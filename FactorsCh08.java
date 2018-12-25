569ef1777aeaa9a5e3e3d065c16db0d53ee695e7
R'n'B <9968920+RnBDeveloper@users.noreply.github.com>
2018-12-25 22:35:01 +0100

    Update FactorsCh08.java

--- FactorsCh08.java
+++ FactorsCh08.java
@@ -4,11 +4,17 @@ import java.util.Arrays;
 public class FactorsCh08 {
 	public static boolean areFactors(int[] x, int n) {
 		int i;
+		int j = 0;
 		int count = 0;
+		int nonfac = 0;
+		int[] b = new int[x.length];
 
 		for (i = 0; i < x.length; i++) {
-			int index = x[i];
-			if (n % index == 0) {
+			if (n % x[i] != 0) {
+				b[j] = x[i];
+				j++;
+				nonfac++;
+			} else {
 				count++;
 			}
 		}
@@ -18,7 +24,10 @@ public class FactorsCh08 {
 		if (count != x.length) {
 			System.out.print("\n\nThis array ");
 			System.out.print(Arrays.toString(x));
-			System.out.print(" contains numbers that are not factors of " + n + ".");
+			System.out.print(" contains numbers that are not factors of " + n + " which are: ");
+			int[] newb = new int[nonfac]; // Creating a new array
+			System.arraycopy(b, 0, newb, 0, newb.length); // Copying the array b into the new created one: newb
+			System.out.print(Arrays.toString(newb));
 			return false;
 		} else {
 			System.out.println("\n\nAll numbers of the array a are factors of " + n + ".");
