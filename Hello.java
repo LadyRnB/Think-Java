aaf4ddaa47f79b789d6e557788a8d00c59e50613
R'n'B <9968920+RnBDeveloper@users.noreply.github.com>
2019-01-12 16:28:33 +0100

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,6 +2,7 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello, World!");
 		System.exit(0); //The method never returns because it's the end of the world
-		//and none of your code is going to be executed next.
+		//& none of your code is going to be executed next.
+		//This is a convenient way to handle shutdown in bigger programs
 	}
 }
