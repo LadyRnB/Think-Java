ec356b4b1a4603dd106cc1d1f960ef04cb14d3a7
R'n'B <9968920+RnBDeveloper@users.noreply.github.com>
2019-12-19 19:48:35 +0100

    Update Convertsecch03.java

--- Convertsecch03.java
+++ Convertsecch03.java
@@ -7,7 +7,7 @@ public static void main(String[] args) {
 	sec = in.nextInt();
 	h = sec / 3600;
 	min = (sec % 3600 ) / 60;
-  remainder = ( sec % 60 );
+        remainder = ( sec % 60 ); // Or remainder = (sec % 3600) % 60; (but that's uselessly complicated
 	System.out.printf("\n%d seconds = %d hours, %d minutes, & %d seconds!", sec, h, min, remainder);
 	in.close();
 }
