ff3774a160277b166fb546ffc2f4e0b128bc3589
R'n'B <9968920+RnBDeveloper@users.noreply.github.com>
2019-12-24 16:36:18 +0100

    Update CheckFermatch05.java

--- CheckFermatch05.java
+++ CheckFermatch05.java
@@ -5,16 +5,17 @@ public class CheckFermatch05 {
 		a = (int) Math.pow(a, n);
 		b = (int) Math.pow(b, n);
 		c = (int) Math.pow(c, n);
-		// note: Better remove type cast, using Type casting is useless here, 
-		//since the user should introduce an integer, otherwise the program
-		//will generate an InputMismatchException
+		// note: Better remove that type cast, Type casting an Integer is useless here,
+		// since the user should introduce an integer, otherwise the program
+		// will generate an InputMismatchException
 		if (n > 2) {
 			if ((a + b) == c) {
 				System.out.print("Holy smokes, Fermat was wrong!" + a + " + " + b + " = " + c);
-
-			} else {
+			} else if ((a + b) != c) {
 				System.out.print("no, that doesn't work, since: " + a + " + " + b + " != " + c);
 			}
+		} else {
+			System.out.print("See, Fermat's theory is correct!");
 		}
 
 	}
@@ -30,8 +31,8 @@ public class CheckFermatch05 {
 		z = in.nextInt();
 		System.out.print("Enter the number p: ");
 		p = in.nextInt();
-		System.out.print((int) Math.pow(x, p) + " + "); //Type cast is useless here as well
-		System.out.print((int) Math.pow(y, p) + " = "); //since all numbers are integers
+		System.out.print((int) Math.pow(x, p) + " + "); // Type cast is useless here as well
+		System.out.print((int) Math.pow(y, p) + " = "); // since all numbers are integers
 		System.out.print((int) Math.pow(x, p) + (int) Math.pow(y, p));
 		System.out.println();
 		checkFermat(x, y, z, p);
